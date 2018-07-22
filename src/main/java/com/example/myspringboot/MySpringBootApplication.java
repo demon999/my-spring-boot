db7e0108eb8d4fed86ddfe79175f8084ddee7465
xueliming <liming.snowy@gmail.com>
2018-07-22 15:34:26 +0800

    init

--- src/main/java/com/example/myspringboot/MySpringBootApplication.java
+++ src/main/java/com/example/myspringboot/MySpringBootApplication.java
@@ -1,13 +1,9 @@
 package com.example.myspringboot;
 
-import com.example.myspringboot.app.DataSourceConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
-@Import(DataSourceConfiguration.class)
 public class MySpringBootApplication {
 
 	public static void main(String[] args) {
